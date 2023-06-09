3e4a8972adfb791b3feea82eb6b2662c8f790b53
engineeranil <120477702+engineeranil@users.noreply.github.com>
2023-06-09 18:09:26 +0300

    SalaryCalculator_JAVA

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javax.xml.namespace.QName;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee em = new Employee("ANIL",2000,45,1985);
+        em.toString();
+    }
+
+}
